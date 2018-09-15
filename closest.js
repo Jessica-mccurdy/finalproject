2a600fdc3fd4ad5251ff452928212cd070a1673b
Jessica-mccurdy <31529643+Jessica-mccurdy@users.noreply.github.com>
2018-09-15 19:29:30 -0400

    Delete closest.js

--- closest.js
+++
@@ -1,9 +0,0 @@
-let form = document.getElementById("closest");
-            form.onsubmit = function() {
-
-                if (!form.rideid.value)
-                {
-                    alert("please choose a ride");
-                    return false;
-                }
-            };
